6cbfbcd3dda30e44a845f9321c7e57a415fd797e
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2025-02-22 18:52:55 +0700

    feat: implement wallet connection functionality and add context provider

--- frontend/src/components/layout/_header.tsx
+++ frontend/src/components/layout/_header.tsx
@@ -1,5 +1,8 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
+"use client";
+import { BrowserWallet } from "@meshsdk/core";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import {
     Dialog,
     DialogContent,
@@ -7,53 +10,87 @@ import {
     DialogHeader,
     DialogTitle,
     DialogTrigger,
-    DialogFooter
-} from "@/components/ui/dialog"
-import Image from "next/image"
-import Link from "next/link"
+    DialogFooter,
+} from "@/components/ui/dialog";
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+
 export default function Header() {
+    const [wallets, setWallets] = useState<any[]>([]);
+    const [isConnected, setIsConnected] = useState<boolean>(true);
+    
+    useEffect(() => {
+        async function fetchWallets() {
+            const availableWallets = await BrowserWallet.getAvailableWallets();
+            setWallets(availableWallets);
+        }
+        fetchWallets();
+    }, []);
+
     return (
         <div className="sticky inset-0  w-full border-b-2  shadow-sm py-3 px-10 bg-white">
             <div className="max-w-6xl flex justify-between items-center mx-auto">
                 <div className="font-semibold text-2xl">
-                    <Link href={"/"}>
-                        NFT Marketplace
-                    </Link>
+                    <Link href={"/"}>NFT Marketplace</Link>
                 </div>
                 <div className="flex items-center space-x-2">
-                    <Dialog>
-                        <DialogTrigger asChild>
-                            <Button className="rounded-3xl">Connect wallet</Button>
-                        </DialogTrigger>
-                        <DialogContent>
-                            <DialogHeader>
-                                <DialogTitle>Available wallets</DialogTitle>
-                            </DialogHeader>
-                            <div>
-                                <Button className="w-full flex items-center justify-start" >
-                                    <Image src={"https://play-lh.googleusercontent.com/BzpWa8LHTBzJq3bxOUjl-Bp7ixh2VOV_5zk6hZjrk57wRp7sc_kvrf3HCrjdKHL_BtbG"}
-                                        width={32} height={32} alt="eternal" />
-                                    <span>Eternal</span>
+                    {isConnected ? (
+                        <Dialog>
+                            <DialogTrigger asChild>
+                                <Button className="rounded-3xl">
+                                    Connect wallet
                                 </Button>
-                            </div>
-                            <DialogFooter>
-                                <DialogClose asChild>
-                                    <Button type="button" variant="secondary">
-                                        Close
-                                    </Button>
-                                </DialogClose>
-                            </DialogFooter>
-                        </DialogContent>
-                    </Dialog>
-                    <Link href={"/account"}>
-                        <Avatar>
-                            <AvatarImage src="https://github.com/shadcn.png" />
-                            <AvatarFallback>HT</AvatarFallback>
-                        </Avatar>
-                    </Link>
-
+                            </DialogTrigger>
+                            <DialogContent>
+                                <DialogHeader>
+                                    <DialogTitle>Available wallets</DialogTitle>
+                                </DialogHeader>
+                                <div className="flex flex-col gap-4">
+                                    {wallets?.map((wallet, key: number) => (
+                                        <Button
+                                            key={key}
+                                            onClick={async () => {
+                                                await BrowserWallet.enable(
+                                                    wallet.id,
+                                                    [95]
+                                                );
+                                                setIsConnected(true);
+                                            }}
+                                            className="w-full flex items-center justify-start"
+                                        >
+                                            <Image
+                                                src={wallet?.icon}
+                                                width={32}
+                                                height={32}
+                                                alt="eternal"
+                                            />
+                                            <span>{wallet.id}</span>
+                                        </Button>
+                                    ))}
+                                </div>
+                                <DialogFooter>
+                                    <DialogClose asChild>
+                                        <Button
+                                            type="button"
+                                            variant="secondary"
+                                        >
+                                            Close
+                                        </Button>
+                                    </DialogClose>
+                                </DialogFooter>
+                            </DialogContent>
+                        </Dialog>
+                    ) : (
+                        <Link href={"/account"}>
+                            <Avatar>
+                                <AvatarImage src="https://github.com/shadcn.png" />
+                                <AvatarFallback>HT</AvatarFallback>
+                            </Avatar>
+                        </Link>
+                    )}
                 </div>
             </div>
         </div>
-    )
+    );
 }
